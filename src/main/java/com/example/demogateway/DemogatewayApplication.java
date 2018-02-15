c710dc30706c4bc22b16fdb2cee32dbacfd79536
Spencer Gibb <spencer@gibb.us>
2018-02-15 14:39:10 -0500

    Update to boot RC1 and Finchley.M6

--- src/main/java/com/example/demogateway/DemogatewayApplication.java
+++ src/main/java/com/example/demogateway/DemogatewayApplication.java
@@ -2,10 +2,9 @@ package com.example.demogateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.filter.factory.RequestRateLimiterGatewayFilterFactory;
 import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.Routes;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
@@ -13,39 +12,32 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
-import static org.springframework.cloud.gateway.filter.factory.GatewayFilters.hystrix;
-import static org.springframework.cloud.gateway.filter.factory.GatewayFilters.rewritePath;
-import static org.springframework.cloud.gateway.handler.predicate.RoutePredicates.host;
-import static org.springframework.cloud.gateway.handler.predicate.RoutePredicates.path;
-
 @SpringBootApplication
 public class DemogatewayApplication {
 
 	@Bean
-	public RouteLocator customRouteLocator(RequestRateLimiterGatewayFilterFactory rateLimiter) {
-		return Routes.locator()
-				.route("path_route")
-					.predicate(path("/get"))
-					.uri("http://httpbin.org:80")
-				.route("host_route")
-					.predicate(host("*.myhost.org"))
-					.uri("http://httpbin.org:80")
-				.route("rewrite_route")
-					.predicate(host("*.rewrite.org"))
-					.filter(rewritePath("/foo/(?<segment>.*)", "/${segment}"))
-					.uri("http://httpbin.org:80")
-				.route("hystrix_route")
-					.predicate(host("*.hystrix.org"))
-					.filter(hystrix("slowcmd"))
-					.uri("http://httpbin.org:80")
-				.route("limit_route")
-					.predicate(host("*.limited.org").and(path("/anything/**")))
-					.filter(rateLimiter.apply(RedisRateLimiter.args(1, 2)))
-					.uri("http://httpbin.org:80")
-				.route("websocket_route")
-					.predicate(path("/echo"))
-					.uri("ws://localhost:9000")
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		//@formatter:off
+		return builder.routes()
+				.route("path_route", r -> r.path("/get")
+						.uri("http://httpbin.org:80"))
+				.route("host_route", r -> r.host("*.myhost.org")
+						.uri("http://httpbin.org:80"))
+				.route("rewrite_route", r -> r.host("*.rewrite.org")
+						.filters(f -> f.rewritePath("/foo/(?<segment>.*)",
+								"/${segment}"))
+						.uri("http://httpbin.org:80"))
+				.route("hystrix_route", r -> r.host("*.hystrix.org")
+						.filters(f -> f.hystrix("slowcmd"))
+								.uri("http://httpbin.org:80"))
+				.route("limit_route", r -> r
+					.host("*.limited.org").and().path("/anything/**")
+						.filters(f -> f.requestRateLimiter(RedisRateLimiter.args(1, 2)))
+					.uri("http://httpbin.org:80"))
+				.route("websocket_route", r -> r.path("/echo")
+					.uri("ws://localhost:9000"))
 				.build();
+		//@formatter:on
 	}
 
 	@Bean
