09b13c424fde3a3e3df4d6aac481eb7f1aa75751
Hazar Gul Nazari <hazar.nazari@favendo.com>
2020-06-23 22:40:03 +0200

    refactor code

--- src/main/java/com/example/demogateway/RestAPI.java
+++ src/main/java/com/example/demogateway/RestAPI.java
@@ -1,5 +1,7 @@
 package com.example.demogateway;
 
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +11,12 @@ public class RestAPI {
     @RequestMapping("/hystrixfallback")
     public String hystrixfallback() {
         return "This is a fallback";
+    }
+
+    @GetMapping("/hello")
+    public String hello(ServerHttpRequest request){
+        request.getHeaders().forEach((key, value) -> System.out.println(key + " " + value));
+        return "Hello, from spring boot application";
     }
 
 }
--- src/main/java/com/example/demogateway/Routing.java
+++ src/main/java/com/example/demogateway/Routing.java
@@ -13,26 +13,14 @@ public class Routing {
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         //@formatter:off
         return builder.routes()
-                .route("path_route", r -> r.path("/get")
-                        .uri("http://httpbin.org"))
-                .route("host_route", r -> r.host("*.myhost.org")
-                        .uri("http://httpbin.org"))
-                .route("rewrite_route", r -> r.host("*.rewrite.org")
-                        .filters(f -> f.rewritePath("/foo/(?<segment>.*)",
-                                "/${segment}"))
-                        .uri("http://httpbin.org"))
-                .route("hystrix_route", r -> r.host("*.hystrix.org")
-                        .filters(f -> f.hystrix(c -> c.setName("slowcmd")))
-                        .uri("http://httpbin.org"))
-                .route("hystrix_fallback_route", r -> r.host("*.hystrixfallback.org")
-                        .filters(f -> f.hystrix(c -> c.setName("slowcmd").setFallbackUri("forward:/hystrixfallback")))
-                        .uri("http://httpbin.org"))
-                .route("limit_route", r -> r
-                        .host("*.limited.org").and().path("/anything/**")
-                        .filters(f -> f.requestRateLimiter(c -> c.setRateLimiter(redisRateLimiter())))
-                        .uri("http://httpbin.org"))
-                .route("websocket_route", r -> r.path("/echo")
-                        .uri("ws://localhost:9000"))
+//                .route(p -> p.path("/hello").filters(f -> f.addRequestHeader("hello", "world")).uri("http://localhost:8080"))
+                .route("path_route", r -> r.path("/get").uri("http://httpbin.org"))
+                .route("host_route", r -> r.host("*.myhost.org").uri("http://httpbin.org"))
+                .route("rewrite_route", r -> r.host("*.rewrite.org").filters(f -> f.rewritePath("/foo/(?<segment>.*)","/${segment}")).uri("http://httpbin.org"))
+                .route("hystrix_route", r -> r.host("*.hystrix.org").filters(f -> f.hystrix(c -> c.setName("slowcmd"))).uri("http://httpbin.org"))
+                .route("hystrix_fallback_route", r -> r.host("*.hystrixfallback.org").filters(f -> f.hystrix(c -> c.setName("slowcmd").setFallbackUri("forward:/hystrixfallback"))).uri("http://httpbin.org"))
+                .route("limit_route", r -> r.host("*.limited.org").and().path("/anything/**").filters(f -> f.requestRateLimiter(c -> c.setRateLimiter(redisRateLimiter()))).uri("http://httpbin.org"))
+                .route("websocket_route", r -> r.path("/echo").uri("ws://localhost:9000"))
                 .build();
         //@formatter:on
     }
